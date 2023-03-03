3f532a38fda9d9034c628efff255133f873e254c
Ivan Zafirovski <ivan_zafirovski@yahoo.com>
2023-03-03 20:44:18 +0100

    update js file

--- app.js
+++ app.js
@@ -5,17 +5,33 @@ const overlay = document.querySelector(".overlay");
 const btnClose = document.querySelector(".close-modal");
 const btnsShow = document.querySelectorAll(".show-modal");
 
-for (let i = 0; i < btnsShow.length; i++)
-  btnsShow[i].addEventListener("click", function () {
-    modal.classList.remove("hidden");
-    overlay.classList.remove("hidden");
-  });
+// set the function for more dry code
+const modalClose = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+const modalOpen = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
 
-btnClose.addEventListener("click", function () {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
-});
-overlay.addEventListener("click", function () {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
-});
+for (let i = 0; i < btnsShow.length; i++)
+  btnsShow[i].addEventListener("click", modalOpen);
+
+btnClose.addEventListener("click", modalClose);
+overlay.addEventListener("click", modalClose);
+
+// put the same code in a function expression
+// btnsShow[i].addEventListener("click", function () {
+//   modal.classList.remove("hidden");
+//   overlay.classList.remove("hidden");
+// });
+
+// btnClose.addEventListener("click", function () {
+//   modal.classList.add("hidden");
+//   overlay.classList.add("hidden");
+// });
+// overlay.addEventListener("click", function () {
+//   modal.classList.add("hidden");
+//   overlay.classList.add("hidden");
+// });
