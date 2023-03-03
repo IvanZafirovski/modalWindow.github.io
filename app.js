b1aa63d4062374b36b4acf28fc8f8b97a4339309
Ivan Zafirovski <ivan_zafirovski@yahoo.com>
2023-03-03 21:09:41 +0100

    finished

--- app.js
+++ app.js
@@ -21,6 +21,18 @@ for (let i = 0; i < btnsShow.length; i++)
 btnClose.addEventListener("click", modalClose);
 overlay.addEventListener("click", modalClose);
 
+// adding event parameter in the function to target property
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Escape" && !modal.classList.contains("hidden")) {
+    {
+      // if the modal DOES NOT contains class hidden then close the modal
+      // if (!modal.classList.contains("hidden")) {
+      modalClose();
+    }
+  }
+  console.log("escape wass press");
+});
+
 // put the same code in a function expression
 // btnsShow[i].addEventListener("click", function () {
 //   modal.classList.remove("hidden");
